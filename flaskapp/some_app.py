97527c8b5a04b15b34c5743921b56ac309f930de
Sokolov Pavel <spavels2@mail.ru>
2024-12-22 23:55:20 +0800

    test with new data

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,10 +1,12 @@
-print("Hello world")
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "<html><head></head><body>Hello World!</body></html>"
+@app.route("/data_to")
+def data_to():
+  some_pars = {'user':'Ivan','color':'red'}
+  some_str = 'Hello my dear friends!'
+  some_value = 10
 
-if __name__ == "__main__":
-  app.run(host='127.0.0.1',port=5000)
+  return render_template('simple.html', some_str = some_str,
+	 some_value = some_value, some_pars = some_pars)
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
 from some_app import app
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.run()
